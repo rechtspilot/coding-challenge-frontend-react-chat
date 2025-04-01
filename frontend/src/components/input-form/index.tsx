2d5dc80087ba813a26b73744c9d81fe3c3d74614
Pavel Mareev <mareev.pv@gmail.com>
2025-04-01 18:09:41 +0200

    feat: refocus input on submit

--- frontend/src/components/input-form/index.tsx
+++ frontend/src/components/input-form/index.tsx
@@ -1,6 +1,6 @@
-import { Button, Flex, Form, Input } from "antd";
+import { Button, Flex, Form, Input, InputRef } from "antd";
 import styles from "./index.module.css";
-import { FC } from "react";
+import { FC, useRef } from "react";
 
 const { useForm } = Form;
 
@@ -15,9 +15,12 @@ type FormData = {
 
 export const InputForm: FC<InputFormProps> = ({ onSubmit, loading }) => {
   const [form] = useForm<FormData>();
+  const inputRef = useRef<InputRef>(null);
 
   function handleFormSubmit({ input }: FormData) {
     form.resetFields();
+    setTimeout(() => inputRef.current?.focus(), 100)
+
 
     if (input.trim()) {
       onSubmit(input);
@@ -28,7 +31,7 @@ export const InputForm: FC<InputFormProps> = ({ onSubmit, loading }) => {
     <Form onFinish={handleFormSubmit} form={form}>
       <Flex gap="small">
         <Form.Item<string> className={styles.input} name="input">
-          <Input placeholder="Say hi..." />
+          <Input placeholder="Say hi..." ref={inputRef} />
         </Form.Item>
         <Button type="primary" htmlType="submit" loading={loading}>
           Submit
